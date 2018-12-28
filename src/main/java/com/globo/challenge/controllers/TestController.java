10b5deeed2a4602077227aa01b33b0135ebdc9fe
caiomeriguetti <caiomeriguetti@gmail.com>
2018-12-27 22:53:08 -0200

    useless imports

--- src/main/java/com/globo/challenge/controllers/TestController.java
+++ src/main/java/com/globo/challenge/controllers/TestController.java
@@ -1,15 +1,11 @@
 package com.globo.challenge.controllers;
 
-import com.globo.challenge.models.Test;
-import com.globo.challenge.repository.TestRepository;
 import com.globo.challenge.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
