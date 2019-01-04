af56159d3929216837e11908e7597ab569d29c61
caiomeriguetti <caiomeriguetti@gmail.com>
2019-01-04 21:48:39 -0200

    10k connections

--- 10k/10k.py
+++ 10k/10k.py
@@ -1,42 +1,30 @@
 import requests
+import socket
+import time
 from threading import Thread
 
-report = {
-    "200": 0,
-    "not200": 0
-}
-
 n = 10000
-k = 0
-w = 0
 
-def req():
-    global k
+def opensock():
+    s = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
 
-    r = requests.get('https://localhost:9443/public/sleep', verify=False)
-    if r.status_code == 200:
-        report["200"] += 1
-    else:
-        report["not200"] +=1
+    s.connect(("localhost", 9443))
 
-    k += 1
-
-    print float(k)/float(n)
-
+    while 1:
+        time.sleep(1)
 
 def main():
     threads = []
     for i in range(0, n):
-        t = Thread(target=req)
+        t = Thread(target=opensock)
         threads.append(t)
-
         t.start()
 
     for t in threads:
         t.join()
 
-    print report
-
+    whi
 
 if __name__ == "__main__":
     main()
