83bf303e6af3071f48b420602663242d2bcaf2e3
caiomeriguetti <caiomeriguetti@gmail.com>
2019-01-07 01:28:52 -0200

    xnio

--- src/main/java/com/globo/challenge/controllers/UserController.java
+++ src/main/java/com/globo/challenge/controllers/UserController.java
@@ -4,6 +4,9 @@ import com.globo.challenge.services.AppUserService;
 import com.globo.challenge.models.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.ForkJoinPool;
 
 
 @RestController
@@ -13,10 +16,18 @@ public class UserController {
     AppUserService appUserService;
 
     @RequestMapping(value = "/api/user/{username}", method = RequestMethod.GET)
-    public AppUser getUserByUsername(@PathVariable("username") String username) {
+    public DeferredResult<AppUser> getUserByUsername(@PathVariable("username") String username) {
 
-        AppUser usr = appUserService.getUserByUsername(username);
-        return usr;
+        DeferredResult<AppUser> result = new DeferredResult<>(2000l);
+
+        ForkJoinPool.commonPool().submit(() -> {
+
+            AppUser usr = appUserService.getUserByUsername(username);
+
+            result.setResult(usr);
+        });
+
+        return result;
     }
 
     @RequestMapping(value = "/api/user/{username}/status", method = RequestMethod.PUT)
