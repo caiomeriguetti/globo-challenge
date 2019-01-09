b75afb45bbad9d355642fefc34833ee905a80ff0
caiomeriguetti <caiomeriguetti@gmail.com>
2019-01-08 23:01:14 -0200

    get users

--- frontend/globochallenge/src/app/app.component.ts
+++ frontend/globochallenge/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild} from '@angular/core';
+import { UserService } from './user.service'
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,18 @@ export class AppComponent {
 
   @ViewChild('searchInput') searchInput: ElementRef;
 
+  userService:UserService
+
+  constructor(private userService: UserService) {
+    this.userService = userService;
+  }
+
   onKeydown(event) {
     console.log(event.target.value);
+    var x;
+    this.userService.getUsers()
+     .subscribe((data) => x = data);
+
+    console.log(x);
   }
 }
--- frontend/globochallenge/src/app/user.service.ts
+++ frontend/globochallenge/src/app/user.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class UserService {
+
+  http:HttpClient
+
   constructor(private http: HttpClient) {
-
-
-
+    this.http = http;
   }
 
   getUsers() {
-    
+
+    var headers_object = new HttpHeaders();
+    headers_object.append("Authorization", "Basic " + btoa("caio:1234"));
+
+    const httpOptions = {
+      headers: headers_object
+    };
+
+    return this.http.get("https://localhost:9443/api/user/caio", httpOptions);
   }
 }
